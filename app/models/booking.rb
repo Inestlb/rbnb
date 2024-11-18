099d8559dab8c28e3618571f68dbfd8b7e099d28
Sarah Lepilleur <sarah.lepilleur@sciencespo.fr>
2024-11-18 15:37:27 +0100

    routes & validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :celebrity
+  validates :total_price, presence: true
+  validates :activity, presence: true
 end
--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,3 +1,6 @@
 class Celebrity < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :price_per_hour, presence: true
+  validates :location, presence: true
 end
